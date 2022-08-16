a4615c871c6af05870449a29a6d1c6d063235895
Ahmed Ashraf <ahmedabdou1410@gmail.com>
2022-08-16 11:18:03 +0300

    init server

--- app/routers/file_router.py
+++ app/routers/file_router.py
@@ -6,5 +6,25 @@ from app.models.file import File
 router = APIRouter(prefix='/files')
 
 @router.get("/", tags=['files'])
-async def get_files(file_pk = None):
-    return []
+async def get_files(file_pk=None):
+    if file_pk: return await file_repo.get_file(file_pk)
+    return await file_repo.get_all_files()
+
+@router.post("/", tags=['files'])
+async def upload_file(req: Request):
+    file = await req.json()
+    file = await file_repo.add_file(File(
+        user_id=file['user_id'],
+        file_name=file['file_name'],
+        text=json.dumps(file['text'])
+    ))
+    File.db().expire(name=file.key(), time=600000)
+    return file
+
+@router.get("/{file_pk}", tags=['files'])
+async def get_files():
+    return await file_repo.get_file()
+
+@router.delete("/{file_pk}", tags=['files'])
+async def upload_file(file_pk: str):
+    return await file_repo.delete_file(file_pk)
