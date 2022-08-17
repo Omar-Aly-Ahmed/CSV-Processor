46ad19d321f96161217d705de407670f3cc9cda2
OElsh4 <amrmawaheb65@gmail.com>
2022-08-17 14:43:52 +0300

    Add new table column

--- client/components/TableEntry.jsx
+++ client/components/TableEntry.jsx
@@ -1,8 +1,9 @@
-const TableEntry = ({ id, fileName, result }) => {
+const TableEntry = ({ id, fileName, accuracy, result }) => {
     return (
         <tr className="hover">
             <th>{id}</th>
             <td>{fileName}</td>
+            <td>{accuracy}</td>
             <td>{result}</td>
         </tr>
     );
