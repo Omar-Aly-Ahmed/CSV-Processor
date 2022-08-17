2bd22c9990b9ad390bf9a5124fe43bc72a708d22
alysoliman <alysoliman05@gmail.com>
2022-08-17 16:05:28 +0300

    change download endpoints and add headers

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -1,7 +1,9 @@
-const Button = ({ text }) => {
+const Button = ({ text, clickHandler }) => {
   return (
     <div className="flex justify-center items-center pb-9 ">
-      <button className="btn btn-outline btn-info"> {text} </button>
+      <button className="btn glass" onClick={clickHandler}>
+        {text}
+      </button>
     </div>
   );
 };
